323a4ea6b9914b6303235c2960b41027e87397c3
Prakash Agrahari <56213227+prakash855@users.noreply.github.com>
2026-09-16 07:09:17 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.ts
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        let map={}
+        for(let str of strs){
+            let key = str.split("").sort().join("")
+            if(!map[key])map[key]=[]
+            map[key].push(str)
+        }
+        return Object.values(map)
+    }
+}
