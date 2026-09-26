6f39be81bb239a1f8d4a9c1d82fc39c1233b0e31
Prakash Agrahari <56213227+prakash855@users.noreply.github.com>
2026-09-26 20:37:04 +0530

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+      // Create an empty string to hold sober string (without special char)
+      let str ="", reversedStr=''
+      //Create an regex consition to test if it is sber
+      let regex = /^[A-Za-z0-9]+$/
+      // Iterate over string all with lowercase and get the sober string
+      for(let i=0; i<s.length; i++){
+        if(regex.test(s[i]))str+=s[i].toLowerCase()
+      }
+      // Now compare by reversing
+      for(let i=str.length-1; i>=0; i--)reversedStr+=str[i]
+      return str === reversedStr
+    }
+}
